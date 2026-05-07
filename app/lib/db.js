2d534bb659925f54159173a0e086aad86603acd1
premjadhav204 <premj204@gmail.com>
2026-05-07 17:01:25 +0530

    Update db.js

--- app/lib/db.js
+++ app/lib/db.js
@@ -1,26 +1,14 @@
-// import mysql from "mysql2/promise";
-
-// const db = mysql.createPool({
-//   host: "localhost", 
-//   user: "root",
-//   password: "",     
-//   database: "my_test",
-//   waitForConnections: true,
-//   connectionLimit: 10,
-// });
-
-// export default db;
-
-
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: "216.10.253.53", 
-  user: "interejr_admin_card",
-  password: "interejr_admin_card",     
-  database: "interejr_admin_card",
+  host: "localhost", 
+  user: "root",
+  password: "",     
+  database: "my_test",
   waitForConnections: true,
-  connectionLimit: 100,
+  connectionLimit: 10,
 });
 
 export default db;
+
+
